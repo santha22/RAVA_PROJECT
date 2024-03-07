d414c860f6840eceb09c39f6df737271a410438d
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2024-03-07 17:14:48 +0530

    className

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -26,8 +26,8 @@ const Todo = ({ todo, onDelete, onUpdate }) => {
             </div>
           ) : (
 
-            <div class="card m-2">
-              <div class="card-body">
+            <div className="card m-2">
+              <div className="card-body">
               {todo.title} - {todo.completed ? 'Completed' : 'Not Completed'}
                  <button className='btn btn-primary mx-2' onClick={() => setEditing(true)}>Edit</button>
                  <button className='btn btn-danger' onClick={() => onDelete(todo.id)}>Delete</button>
